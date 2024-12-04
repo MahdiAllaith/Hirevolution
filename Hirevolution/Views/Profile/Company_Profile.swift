60a785702cc4735cde0fd8eb28c80540e6dcadd8
Muntadher10A <muntdr10gtr@gmail.com>
2024-12-04 10:41:33 +0300

    V2.1

---
+++ Hirevolution/Views/Profile/Company_Profile.swift
@@ -0,0 +1,29 @@
+//
+//  Company_Profile.swift
+//  Hirevolution
+//
+//  Created by BP-36-201-22 on 04/12/2024.
+//
+
+import UIKit
+
+class Company_Profile: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
